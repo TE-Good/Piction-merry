7951eae0b2567247043b68542b54e9b8ef217962
Tom Good <tom.good@eigentech.com>
2020-12-18 13:21:40 +0000

    Updates

--- static/js/main.f72d5471.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonppiction-merry"]=this["webpackJsonppiction-merry"]||[]).push([[0],{10:function(e,n,t){},11:function(e,n,t){"use strict";t.r(n);var c=t(0),r=t(1),i=t.n(r),o=t(3),s=t.n(o);t(9),t(10);var p=function(){return Object(c.jsx)("div",{className:"App",children:"hello"})};s.a.render(Object(c.jsx)(i.a.StrictMode,{children:Object(c.jsx)(p,{})}),document.getElementById("root"))},9:function(e,n,t){}},[[11,1,2]]]);
-//# sourceMappingURL=main.f72d5471.chunk.js.map
