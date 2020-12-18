deefffc5d75e2bb9a86707b20ac253a8072d8392
Tom Good <tom.good@eigentech.com>
2020-12-18 14:14:33 +0000

    Inital commit with canvas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useRef } from "react";
+import { Stage, Layer, Line, Text } from "react-konva";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [tool, setTool] = useState("pen");
+    const [lines, setLines] = useState([]);
+    const isDrawing = useRef(false);
+
+    const handleMouseDown = (e) => {
+        isDrawing.current = true;
+        const pos = e.target.getStage().getPointerPosition();
+        setLines([...lines, { tool, points: [pos.x, pos.y] }]);
+    };
+
+    const handleMouseMove = (e) => {
+        // no drawing - skipping
+        if (!isDrawing.current) return;
+
+        const stage = e.target.getStage();
+        const point = stage.getPointerPosition();
+        let lastLine = lines[lines.length - 1];
+
+        // add point
+        lastLine.points = lastLine.points.concat([point.x, point.y]);
+
+        // replace last
+        lines.splice(lines.length - 1, 1, lastLine);
+        setLines(lines.concat());
+    };
+
+    const handleMouseUp = () => (isDrawing.current = false);
+
+    return (
+        <div className="App">
+          <div className="stage-container">
+            <Stage
+                width={300}
+                height={300}
+                onMouseDown={handleMouseDown}
+                onMouseMove={handleMouseMove}
+                onMouseUp={handleMouseUp}
+            >
+                <Layer>
+                    <Text text="Just start drawing" x={5} y={30}></Text>
+                    {lines.map((line, i) => (
+                        <Line
+                            key={i}
+                            points={line.points}
+                            stroke="#df4b26"
+                            strokeWidth={5}
+                            tension={0.5}
+                            lineCap="round"
+                            // globalCompositeOperation={
+                            //     line.tool === "eraser"
+                            //         ? "destination-out"
+                            //         : "source-over"
+                            // }
+                        />
+                    ))}
+                </Layer>
+            </Stage>
+          </div>
+            {/* <select value={tool} onChange={(e) => setTool(e.target.value)}>
+                <option value="pen">Pen</option>
+                <option value="eraser">Eraser</option>
+            </select> */}
+        </div>
+    );
 }
 
 export default App;
